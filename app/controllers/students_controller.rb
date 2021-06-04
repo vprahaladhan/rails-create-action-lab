905ae8b00db5829b6cbea9db429ec6f5ecc5c61f
prahal <vprahaladhan@gmail.com>
2021-06-04 14:44:48 +1000

    commit tree 1240edf656be2d8472b0da16bf57397145145a7b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,15 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    redirect_to @student
   end
 
 end
